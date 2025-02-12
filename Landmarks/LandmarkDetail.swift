4b12aef87bf1515c373a7376d26611f8f085313c
Emerson <emerson.day@gmail.com>
2025-02-11 19:49:15 -0800

    first commit

---
+++ Landmarks/LandmarkDetail.swift
@@ -0,0 +1,55 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Emerson Day on 2/8/25.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+    var body: some View {
+        
+        VStack {
+            MapView()
+                .frame(height: 300)
+
+
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+
+
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+
+
+                Divider()
+
+
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+
+
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkDetail(landmark: landmarks[0])
+}
