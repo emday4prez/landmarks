f6a3e62e32ca3fc8a8daf43b63350b284dd9a674
Emerson <emerson.day@gmail.com>
2025-03-08 12:53:31 -0800

    drawing shapes and paths

---
+++ Landmarks/Views/Badges/Badge.swift
@@ -0,0 +1,31 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Emerson Day on 3/8/25.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var badgeSymbols: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 0))
+            .opacity(0.5)
+    }
+    
+    var body: some View {
+        ZStack {
+            BadgeBackground()
+            
+            GeometryReader { geometry in
+                badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+            }
+        }
+        .scaledToFit()
+    }
+}
+#Preview {
+    Badge()
+}
