031c1b8777e95310c2c6055d359cdab1dd554404
Emerson <emerson.day@gmail.com>
2025-03-15 09:41:30 -0700

    category row

---
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,32 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+            HStack(alignment: .top, spacing: 0) {
+                ForEach(items) { landmark in
+                    CategoryItem(landmark: landmark)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    let landmarks = ModelData().landmarks
+    return CategoryRow(
+        categoryName: landmarks[0].category.rawValue,
+        items: Array(landmarks.prefix(3))
+    )
+}
