e10cb03984ca0d22188ca9a1de50137eddddf398
Emerson <emerson.day@gmail.com>
2025-03-31 19:15:45 -0700

    display profile

--- Landmarks/Views/Profiles/ProfileSummary.swift
+++ Landmarks/Views/Profiles/ProfileSummary.swift
@@ -6,6 +6,7 @@ import SwiftUI
 
 
 struct ProfileSummary: View {
+    @Environment(ModelData.self) var modelData
     var profile: Profile
 
 
@@ -20,6 +21,37 @@ struct ProfileSummary: View {
                 Text("Notifications: \(profile.prefersNotifications ? "On": "Off" )")
                 Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
                 Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+                
+                Divider()
+
+
+                VStack(alignment: .leading) {
+                    Text("Completed Badges")
+                        .font(.headline)
+
+
+                    ScrollView(.horizontal) {
+                        HStack {
+                            HikeBadge(name: "First Hike")
+                            HikeBadge(name: "Earth Day")
+                                .hueRotation(Angle(degrees: 90))
+                            HikeBadge(name: "Tenth Hike")
+                                .grayscale(0.5)
+                                .hueRotation(Angle(degrees: 45))
+                        }
+                        .padding(.bottom)
+                    }
+                }
+                Divider()
+
+
+                VStack(alignment: .leading) {
+                    Text("Recent Hikes")
+                        .font(.headline)
+
+
+                    HikeView(hike: modelData.hikes[0])
+                }
             }
         }
     }
