b0e760d1bfcbb89e0eebbc2164eff1b8be62879e
Emerson <emerson.day@gmail.com>
2025-04-10 15:51:59 -0700

    watch target

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Emerson Day on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
